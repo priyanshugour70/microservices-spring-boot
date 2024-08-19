42f3708606dc207d60faa40faae4501f948ff71b
Priyanshu Gour <priyanshugour1@gmail.com>
2024-08-19 13:13:13 +0530

    Update

--- 01-microservice-rest-templete/springprovider-app/src/test/java/com/gour/priyanshu/SpringProviderAppApplicationTests.java
+++ 01-microservice-rest-templete/springprovider-app/src/test/java/com/gour/priyanshu/SpringProviderAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringproviderAppApplicationTests {
+class SpringProviderAppApplicationTests {
 
 	@Test
 	void contextLoads() {
